b4cfee04e02ec0616dfc1b1c71e9af9d03ce8047
Ocolisan Rares <32493420+ocolisanrares97@users.noreply.github.com>
2019-05-01 21:41:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,90 @@
 from helperFunctions import *
 import matplotlib.pyplot as plt
+from generator import *
 
 
 
-equations = getFileContent("sistem.txt")
+def boot(filename):
 
-coefficients, freeTerms = getCoefficientsAndFreeTerms(equations)
+    equations = getFileContent(filename)
+    coefficients, freeTerms = getCoefficientsAndFreeTerms(equations)
 
-population_size = 2500
-crossover_rate = 0.50 #25% chance to use current member for crossover
-mutation_rate = 0.2 #10% chance to mutate
-no_of_variables = len(coefficients[0])
+    population_size = 1000
+    crossover_rate = 0.50 #25% chance to use current member for crossover
+    mutation_rate = 0.2 #10% chance to mutate
+    no_of_variables = len(coefficients[0])
 
-population = initializePopulation(population_size , no_of_variables)
+    population = initializePopulation(population_size , no_of_variables)
 
-# sol = [1,2,3]
-#
-#
-# for i in range(0,10000):
-#     imporve_solution(sol)
-#     print(sol)
-# print(sol)
-next_generation = population
+    next_generation = population
+    #
+    # gen = Generator()
+    # gen.print_sistem()
+    bestFit = 3000
+    while bestFit > 0.3:
 
+        previous_generation = next_generation.copy()
 
-bestFit = 1000
-while bestFit > 0.05:
+        ''' compute the fitness value for each solution '''
+        fitnessList = []
+        for pop in next_generation:
+            fitnessList.append(calcFitnessAbsValue(coefficients, freeTerms, pop))
 
-    previous_generation = next_generation.copy()
+        fitList2 = []
+        for pop in next_generation:
+            fitList2.append(calcFitnessSquare(coefficients,freeTerms,pop))
 
-    ''' compute the fitness value for each solution '''
-    fitnessList = []
-    for pop in next_generation:
-        fitnessList.append(calcFitnessAbsValue(coefficients, freeTerms, pop))
-
-    fitList2 = []
-    for pop in next_generation:
-        fitList2.append(calcFitnessSquare(coefficients,freeTerms,pop))
-
-    #K =[]
-    #selected_chromosomes = mating_selection(population,K,population_size,fitnessList,fitList2)
-    ''' select the best solutions for the next generation '''
-    selected_chromosomes = roulette_wheel_selection(fitnessList, next_generation)
+        #K =[]
+        #selected_chromosomes = mating_selection(population,K,population_size,fitnessList,fitList2)
+        ''' select the best solutions for the next generation '''
+        selected_chromosomes = roulette_wheel_selection(fitnessList, next_generation)
 
 
-    ''' apply the crossover on the selected chromosomes '''
-    population_after_crossover = crossover(selected_chromosomes,crossover_rate)
+        ''' apply the crossover on the selected chromosomes '''
+        population_after_crossover = crossover(selected_chromosomes,crossover_rate,no_of_variables,coefficients,freeTerms)
 
 
 
-    ''' mutate the population after crossover '''
-    next_generation = mutate(population_after_crossover, mutation_rate)
+        ''' mutate the population after crossover '''
+        next_generation = mutate(population_after_crossover, mutation_rate,population_size,no_of_variables)
 
 
-    # fit_Previous_gen = []
-    # for sol in previous_generation:
-    #     fit_Previous_gen.append(calcFitnessAbsValue(coefficients,freeTerms,sol))
 
-    fit_Next_gen = []
-    for sol in next_generation:
-        fit_Next_gen.append(calcFitnessAbsValue(coefficients,freeTerms,sol))
+        fit_Previous_gen = []
+        for sol in previous_generation:
+            fit_Previous_gen.append(calcFitnessAbsValue(coefficients, freeTerms, sol))
+
+        fit_Next_gen = []
+        for sol in next_generation:
+            fit_Next_gen.append(calcFitnessAbsValue(coefficients, freeTerms, sol))
+
+        # best_sol_previous = find_best_solution(previous_generation)
+        # best_sol_next = find_best_solution(next_generation)
+
+        bestFitPrev = min(fit_Previous_gen)
+        bestFitNew = min(fit_Next_gen)
+
+        bestFit = bestFitNew
+        if bestFitPrev < bestFitNew:
+            for i in range(0,len(next_generation)):
+                next_generation[i] = previous_generation[i]
+            bestFit = bestFitPrev
+
+        best_sol = find_best_solution(next_generation, coefficients,freeTerms)
+        for i in range(0,2000):
+            imporve_solution(best_sol,coefficients,freeTerms)
+        final_bestFitness = bestFit
+        print(bestFit)
+        #print(find_best_solution(next_generation))
+
+    sol = find_best_solution(next_generation, coefficients, freeTerms)
+    print(sol)
+    final_solution = []
+    for i in range(0,len(sol)):
+        final_solution.append(sol[i])
+
+    return final_solution, final_bestFitness
 
 
-    # best_sol_previous = find_best_solution(previous_generation)
-    best_sol_next = find_best_solution(next_generation)
 
-    for i in range(0,10000):
-        imporve_solution(best_sol_next)
-
-    # bestFitPrev = min(fit_Previous_gen)
-
-    # bestFitNew = min(fit_Next_gen)
-    # bestFit = bestFitNew
-    # if bestFitPrev < bestFitNew:
-    #     next_generation = previous_generation.copy()
-    #     bestFit = bestFitPrev
-
-    bestFit = min(fit_Next_gen)
-    print(bestFit)
-    #print(find_best_solution(next_generation))
-
-sol = find_best_solution(next_generation)
-print(sol)
-
-# K = []
-# Q = mating_selection(population,K,population_size,fitList1,fitList2)
-#
-# print(Q)
 
