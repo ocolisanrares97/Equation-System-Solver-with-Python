429343eba528d09b7897027982e3e3b2e7eea79b
Ocolisan Rares <32493420+ocolisanrares97@users.noreply.github.com>
2019-06-06 16:02:36 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,58 +1,138 @@
 from main import *
 from tkinter import *
+import threading
+dead = False
 
 
+thread = threading.Thread()
 #____BUTTON COMMANDS________
+
+
 def startComputation():
+
+
     str = ""
     filename = inputTextField.get("1.0", END)
     str = filename.strip()
 
+#If the "Use default values" box is checked we set the default parameters values
+    if(checkBoxValue.get() == 1):
+        try:
 
-    try:
-        crossRate = float(crossoverRateTextField.get("1.0", END))
+            if(radioButtuonValue.get() == 1):
 
+                final_solution, fit, execTime = bootBasic(str, 1000, 0.5, 0.2, 0.4, 0.003)
 
-        mutationRate = float(mutationRateTextField.get("1.0", END))
+            elif(radioButtuonValue.get() == 2):
 
-        fitness = float(fitnessTextField.get("1.0", END))
+                final_solution, fit, execTime = bootImproved(str, 1000, 0.5, 0.2, 0.4, 0.003)
 
-        popSize = int(populationSizeTextField.get("1.0", END))
+            else:
+                resultsTextField.delete("1.0", END)
+                resultsTextField.insert(END, "Please select which algorithm you wish to use.")
+                return
 
-        precision = float(precisionTextField.get("1.0", END))
+            crossoverRateTextField.delete("1.0", END)
+            crossoverRateTextField.insert(END, 0.5)
 
-    except:
-        resultsTextField.delete("1.0", END)
-        resultsTextField.insert(END, "Please make sure that you completed the parameter fields.")
-        return
+            mutationRateTextField.delete("1.0", END)
+            mutationRateTextField.insert(END, 0.2)
 
+            fitnessTextField.delete("1.0", END)
+            fitnessTextField.insert(END, 0.1)
 
+            populationSizeTextField.delete("1.0", END)
+            populationSizeTextField.insert(END, 1000)
 
-    try:
-        final_solution,fit = boot(str,popSize,crossRate,mutationRate,fitness,precision)
+            precisionTextField.delete("1.0", END)
+            precisionTextField.insert(END, 0.003)
 
-        resultsTextField.delete("1.0",END)
-        sol = "x"
-        egal = " = "
-        backs = "\n"
+            sol = "x"
+            egal = " = "
+            backs = "\n"
 
-        resultsTextField.insert(END, "Fitness values of the solution is: ")
-        resultsTextField.insert(END, fit)
-        resultsTextField.insert(END, backs)
-        for i in range(0,len(final_solution)):
-
-            resultsTextField.insert(END, sol)
-            resultsTextField.insert(END,i+1)
-            resultsTextField.insert(END, egal)
-            resultsTextField.insert(END, final_solution[i])
+            resultsTextField.delete("1.0", END)
+            resultsTextField.insert(END, "Fitness values of the solution is: ")
+            resultsTextField.insert(END, fit)
             resultsTextField.insert(END, backs)
+            for i in range(0, len(final_solution)):
+                resultsTextField.insert(END, sol)
+                resultsTextField.insert(END, i + 1)
+                resultsTextField.insert(END, egal)
+                resultsTextField.insert(END, final_solution[i])
+                resultsTextField.insert(END, backs)
 
-        del final_solution[:]
-    except:
-        resultsTextField.delete("1.0",END)
-        resultsTextField.insert(END,"Error. Something went wrong when proccesing the solution"
-                                    "  \nMake sure that you have succesfully read the file."
-                                    "\nCheck if you have filled the paramaters fields")
+            resultsTextField.insert(END, "Execution time: ")
+            resultsTextField.insert(END, execTime)
+            del final_solution[:]
+        except:
+            resultsTextField.delete("1.0", END)
+            resultsTextField.insert(END, "Error. Something went wrong when proccesing the solution"
+                                         "  \nMake sure that you have succesfully read the file."
+                                         "\nCheck if you have filled the paramaters fields")
+
+#If the "Use default parameters" is not checked we take the values from the parameters fields
+    else:
+        try:
+            crossRate = float(crossoverRateTextField.get("1.0", END))
+
+
+            mutationRate = float(mutationRateTextField.get("1.0", END))
+
+            fitness = float(fitnessTextField.get("1.0", END))
+
+            popSize = int(populationSizeTextField.get("1.0", END))
+
+            precision = float(precisionTextField.get("1.0", END))
+
+        except:
+            resultsTextField.delete("1.0", END)
+            resultsTextField.insert(END, "Please make sure that you completed the parameter fields.")
+            return
+
+
+
+        try:
+
+            if(radioButtuonValue.get() == 1):
+
+                final_solution,fit, execTime = bootBasic(str,popSize,crossRate,mutationRate,fitness,precision)
+
+            elif (radioButtuonValue.get() == 2):
+
+                final_solution, fit, execTime = bootImproved(str,popSize,crossRate,mutationRate,fitness,precision)
+
+            else:
+                resultsTextField.delete("1.0", END)
+                resultsTextField.insert(END, "Please select which algorithm you wish to use.")
+                return
+
+            resultsTextField.delete("1.0",END)
+            sol = "x"
+            egal = " = "
+            backs = "\n"
+
+            resultsTextField.delete("1.0", END)
+            resultsTextField.insert(END, "Fitness values of the solution is: ")
+            resultsTextField.insert(END, fit)
+            resultsTextField.insert(END, backs)
+            for i in range(0,len(final_solution)):
+
+                resultsTextField.insert(END, sol)
+                resultsTextField.insert(END,i+1)
+                resultsTextField.insert(END, egal)
+                resultsTextField.insert(END, final_solution[i])
+                resultsTextField.insert(END, backs)
+
+            resultsTextField.insert(END, "Execution time: ")
+            resultsTextField.insert(END, execTime)
+
+            del final_solution[:]
+        except:
+            resultsTextField.delete("1.0",END)
+            resultsTextField.insert(END,"Error. Something went wrong when proccesing the solution"
+                                        "  \nMake sure that you have succesfully read the file."
+                                        "\nCheck if you have filled the paramaters fields")
 
 
 def displaySystem():
@@ -80,7 +160,10 @@ def displaySystem():
 
 top = Tk()
 top.title("Equations System Solver")
-top.geometry("1400x500")
+top.geometry("1600x500")
+
+checkBoxValue = IntVar()
+radioButtuonValue = IntVar()
 
 #++++++++++++   WIDGETS   +++++
 
@@ -91,8 +174,8 @@ S = Scrollbar(top)
 
 #____TEXT FIELDS____
 inputTextField = Text(top, height=1, bd=3,width=60)
-resultsTextField = Text(top, height=20, width=60)
-showSystemTextField = Text(top,height=20,width =60)
+resultsTextField = Text(top, height=20,bd=3, width=60)
+showSystemTextField = Text(top,height=20,bd=3,width =60)
 
 
 
@@ -106,8 +189,10 @@ fitnessTextField = Text(top,bd = 2,width=10,height = 1)
 #_____BUTTONS____
 readFileButton = Button(top, text="Read File", command= displaySystem)
 startButton = Button(top, text ="Find Solutions", command = startComputation)
-
-
+#stopButton = Button(top,text = "Stop",width=10)
+defaultValuesCheckbox = Checkbutton(top, text ="Use default parameter values", variable = checkBoxValue,height = 4)
+normalGA_RadioButton = Radiobutton(top,text = "Use Basic Genetic Algorithm", value=1, variable = radioButtuonValue)
+improvedGA_RadioButton = Radiobutton(top,text = "Use Improved Genetic Algorithm", value=2, variable = radioButtuonValue)
 
 #-------------LAYOUT------------
 entryLable.grid(row=0,column=2, sticky=W,pady=10,padx = 5)
@@ -136,6 +221,11 @@ startButton.grid(row=2,column = 4,padx = 20)
 resultsTextField.grid(row=1,column = 5, rowspan=4)
 Label(top,text="Solutions").grid(row=5,column=5,pady=5)
 
+defaultValuesCheckbox.grid(row=6,column=1,pady=10)
 
+normalGA_RadioButton.grid(row=6, column = 3, sticky=E)
+improvedGA_RadioButton.grid(row=6, column = 5, sticky =W)
+
+#stopButton.grid(row=3,column=4,padx=20)
 
 top.mainloop()
